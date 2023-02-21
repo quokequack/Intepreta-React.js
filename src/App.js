d9aaa8aed497622ec0ebf92f68f1bcb40e93f08f
quokequack <roymustangtaizo@gmail.com>
2023-02-21 19:37:56 -0300

    added styles at register and login

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFeather, faMoon } from "@fortawesome/free-solid-svg-icons";
 
-
+library.add(faFeather, faMoon);
 function App() {
   return (
     <>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import '../styles/login.css';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {Link} from 'react-router-dom';
 
 export default function Homepage(){
     return(
@@ -7,7 +9,27 @@ export default function Homepage(){
             <body className='login'>
                 <div className='container login-container'>
                     <div className='container login-container2'>
-                        <h1> Essa é a página de login </h1>
+                        <div className='header'>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon='feather' />
+                            </div>
+                            <h3>Interpreta</h3>
+                        </div>
+                        <div className='form'>
+                            <form className='form-horizontal'>
+                                <label htmlFor='name'>Nome:</label>
+                                <input type='text' name='name' class='form-control'/>
+                                <label htmlFor='name'>Email:</label>
+                                <input type='email' name='name' class='form-control'/>
+                                <label htmlFor='name'>Senha:</label>
+                                <input type='password' name='name' class='form-control'/><br/> 
+                            </form>                           
+                            <div className='register'>
+                                <button type='submit' className='btn btn-primary'>Fazer cadastro!</button> 
+                                <p>Já possui uma conta?</p>
+                                <button id='btn-reg' className='btn btn-primary'><Link className='link' to='/register'>Entre!</Link></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </body>
