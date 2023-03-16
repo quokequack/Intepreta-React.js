cf091231f0fbc3fb5609eb7c967406e6f3c18bb8
quokequack <roymustangtaizo@gmail.com>
2023-03-15 21:16:46 -0300

    cicles page completed

--- src/pages/Cicles.js
+++ src/pages/Cicles.js
@@ -9,22 +9,19 @@ export default function Cicles(){
     useEffect(() => {
         document.title = "Interpreta | Ciclos";
       }, []);
+
     return (
         <>
             <div className='cicles'>
                 <Header />
                 <div className='container cicles-container'>
                    <div className='container-fluid cicle-right'>
-                        <div className='container cicle-img'></div>
+                        <div className='container cicle-img-unblocked'>
+                            <button id='go_button' className='btn go_button'>Começar</button>
+                        </div>
                         <div className='container cicle-text'>
                             <h3 className='text-start'>Ciclo 1</h3>
-                            <p> text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text 
+                            <p> Em alguns casos, a paráfrase é considerada um mecanismo que ajuda a clarificar alguns textos, pois ajuda a explicá-los com termos e expressões que sejam mais simples de compreender, uma vez que a ideia central do conteúdo sempre é mantida. 
                             </p>
                         </div>
                    </div>
@@ -33,70 +30,50 @@ export default function Cicles(){
                             <div className='icon-lock'>
                                 <FontAwesomeIcon icon='lock'/>
                             </div>
+                            <button id='go_button' className='btn btn-primary'>Começar</button>
                         </div>
                         <div className='container cicle-text'>
-                            <h3>Ciclo 1</h3>
-                            <p> text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text 
+                            <h3>Ciclo 2</h3>
+                            <p> A interpretação do texto é um processo de leitura e releitura do texto. Isso porque muitas vezes perdemos alguns dados durante a primeira leitura, então é natural que precisemos reler o texto duas ou até mais vezes, com bastante atenção, para captarmos suas principais ideias.
                             </p>
                         </div>
                    </div>
                    <div className='container-fluid cicle-right'>
                         <div className='container cicle-img'>
-                        <div className='icon-lock'>
+                            <div className='icon-lock'>
                                 <FontAwesomeIcon icon='lock'/>
                             </div>
+                            <button id='go_button' className='btn btn-primary'>Começar</button>
                         </div>
                         <div className='container cicle-text'>
-                            <h3 className='text-start'>Ciclo 1</h3>
-                            <p> text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text 
+                            <h3 className='text-start'>Ciclo 3</h3>
+                            <p> É importante conhecer o significado das palavras e seus sinônimos porque é fundamental para podermos interpretar o mundo que nos rodeia. Sem sabermos o significado das palavras e seus sinônimos estamos, linguisticamente falando, em desvantagem perante os outros e mais dificilmente criaremos laços sociais.                         
                             </p>
                         </div>
                    </div>
                    <div className='container-fluid cicle-left'>
                         <div className='container cicle-img'>
-                        <div className='icon-lock'>
+                            <div className='icon-lock'>
                                 <FontAwesomeIcon icon='lock'/>
                             </div>
+                            <button id='go_button' className='btn btn-primary'>Começar</button>
                         </div>
                         <div className='container cicle-text'>
-                            <h3>Ciclo 1</h3>
-                            <p> text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text 
+                            <h3>Ciclo 4</h3>
+                            <p> Se estiver lendo um livro cujo conteúdo cai no vestibular, evite mexer no celular enquanto está se dedicando à essa leitura. O mesmo vale para a TV e outras distrações. Tente focar sua atenção numa coisa de cada vez, especialmente quando está lendo. 
                             </p>
-                        </div>
+                        </div> 
                    </div>
                    <div className='container-fluid cicle-right'>
                         <div className='container cicle-img'>
-                        <div className='icon-lock'>
+                            <div className='icon-lock'>
                                 <FontAwesomeIcon icon='lock'/>
                             </div>
+                            <button id='go_button' className='btn btn-primary'>Começar</button>
                         </div>
                         <div className='container cicle-text'>
-                            <h3 className='text-start'>Ciclo 1</h3>
-                            <p> text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text text text text text
-                                text text text text text text text text text etxt text text
-                                text text text text text text text text 
+                            <h3 className='text-start'>Ciclo 5</h3>
+                            <p> A primeira leitura do texto deverá ser feita com calma e sem interrupções. No primeiro contato com o texto, o mais importante é tentar compreender o sentido global do texto e identificar o seu objetivo. Não é essencial entender a totalidade do texto, nem o significado de todas as palavras. 
                             </p>
                         </div>
                    </div>
